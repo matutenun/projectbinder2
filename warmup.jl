65960b4e94419c992dff8e461d7a20eec279d03d
matutenun <matias.nunez2@gmail.com>
2021-05-17 03:39:55 -0300

    Add files via upload

--- warmup.jl
+++ warmup.jl
@@ -1,11 +1,8 @@
 
 using DifferentialEquations
 using Plots
-using LinearAlgebra
 
-a=3.0
-f(y,a) =   sign(y)  + a -  y
-f(y,a,t) = f(y,a)
+f(y,t) = y*y  + 3  
 
 sol = solve(  ODEProblem( f, 3, (0,10.0), 0.2 ))
 plot( sol , legend=false , ylims=(-7,7))
