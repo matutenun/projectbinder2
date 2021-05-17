84beec1191054a7d60dc76ab598190b2529c3021
matutenun <matias.nunez2@gmail.com>
2021-05-17 03:10:47 -0300

    Add files via upload

--- warmup.jl
+++ warmup.jl
@@ -1,11 +1,13 @@
 
-using DifferentialEquations, Plots
+using DifferentialEquations
+using Plots
+using LinearAlgebra
 
 a=3.0
 f(y,a) =   sign(y)  + a -  y
-f(y,a,t) = f(y,a) # just for the difeq solver
+f(y,a,t) = f(y,a)
 
 sol = solve(  ODEProblem( f, 3, (0,10.0), 0.2 ));
-plot( sol , legend=false, background_color_inside=:black , ylims=(-7,7), lw=3, c=:red)
+plot( sol , legend=false , ylims=(-7,7))
 
 
