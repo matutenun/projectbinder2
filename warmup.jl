934707e8b2c2dd56f7f7c12bec3a722d9853614f
matutenun <matias.nunez2@gmail.com>
2021-05-17 03:12:11 -0300

    Add files via upload

--- warmup.jl
+++ warmup.jl
@@ -7,7 +7,7 @@ a=3.0
 f(y,a) =   sign(y)  + a -  y
 f(y,a,t) = f(y,a)
 
-sol = solve(  ODEProblem( f, 3, (0,10.0), 0.2 ));
+sol = solve(  ODEProblem( f, 3, (0,10.0), 0.2 ))
 plot( sol , legend=false , ylims=(-7,7))
 
 
